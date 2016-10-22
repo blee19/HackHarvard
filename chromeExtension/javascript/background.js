06e531c3672c7865acb07e6d48d68bf6478542ed
GunnarHorve <gunnarhorve@gmail.com>
2016-10-22 03:31:20 -0400

    sending history to server

--- chromeExtension/javascript/background.js
+++ chromeExtension/javascript/background.js
@@ -5,6 +5,19 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     var hackathonStartTime = 1477094400000; //in ms
     chrome.history.search({text:'', startTime: hackathonStartTime, maxResults:10000, }, function(history) {
       console.log(history);
+
+      xhr = new XMLHttpRequest();
+      var url = "http://52.70.98.235:8000";
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+              console.log("message sent!");
+          }
+          chrome.tabs.create({"url": "http://52.70.98.235"});
+      }
+      var data = JSON.stringify(history);
+      xhr.send(data);
     });
   });
 });
