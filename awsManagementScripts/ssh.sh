265fb3a3c752f312300d50f9ca01b26a13f5f1fa
mahavir24 <abhishek.shah@columbia.edu>
2016-10-22 15:15:10 +0000

    added awsManagementScripts

---
+++ awsManagementScripts/ssh.sh
@@ -0,0 +1,1 @@
+ssh -i ../../hackharvard.pem ubuntu@ec2-52-70-98-235.compute-1.amazonaws.com
---
+++ awsManagementScripts/updateWebsite.sh
@@ -0,0 +1,1 @@
+rsync -rave "ssh -i ../../hackharvard.pem" ../* ubuntu@ec2-52-70-98-235.compute-1.amazonaws.com:/home/ubuntu/hackharvard
