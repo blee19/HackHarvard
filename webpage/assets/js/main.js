a2f397e56ca78fc4b1709a4339f6ab774d581576
mahavir24 <abhishek.shah@columbia.edu>
2016-10-23 06:06:21 +0000

    site now have appropriate picture references

--- webpage/assets/js/main.js
+++ webpage/assets/js/main.js
@@ -130,6 +130,24 @@
 
 				});
 
+				$window.on('load', function() {
+
+					$('#three').poptrox({
+						caption: function($a) { return $a.next('h3').text(); },
+						overlayColor: '#2c2c2c',
+						overlayOpacity: 0.85,
+						popupCloserText: '',
+						popupLoaderText: '',
+						selector: '.work-item a.image',
+						usePopupCaption: true,
+						usePopupDefaultStyling: false,
+						usePopupEasyClose: false,
+						usePopupNav: true,
+						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
+					});
+
+				});
+
 	});
 
 })(jQuery);
